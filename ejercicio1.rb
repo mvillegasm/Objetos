fa023d46b0783c73d716fcb24c761c7ccde3d580
Marcos Villegas <dinavers@gmail.com>
2019-06-11 20:05:31 -0400

    ejercicio1

---
+++ ejercicio1.rb
@@ -0,0 +1,31 @@
+
+# class Anything
+#   def foo
+#     a = 5
+#   end
+
+#   def bar
+#     @a += 1
+#   end
+# end
+
+# any = Anything.new
+# any.foo
+# any.bar
+# any.a
+
+class Anything
+  attr_reader :a
+  def foo
+    @a = 5
+  end
+
+  def bar
+    @a += 1
+  end
+end
+
+any = Anything.new
+any.foo
+any.bar
+any.a
